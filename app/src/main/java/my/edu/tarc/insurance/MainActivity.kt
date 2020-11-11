6d3386bed3bccc6d6bec9192e34bbd2448c08a1b
Ong Yi Jie <ongyj-wp18@student.tarc.edu.my>
2020-11-11 11:16:55 +0800

    2nd commit

--- app/src/main/java/my/edu/tarc/insurance/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/insurance/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import java.util.*
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     // Global variable
@@ -12,41 +13,79 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private var smokerPremium: Int = 0
     private var total: Int = 0
 
+    private lateinit var textViewPremium: TextView
+    private lateinit var textViewMalePremium: TextView
+    private lateinit var textViewSmokerPremium: TextView
+    private lateinit var textViewTotal: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Link UI to program
         val spinnerAge: Spinner = findViewById(R.id.spinnerAge)
-        val radioButtonMale: RadioButton = findViewById(R.id.radioButtonMale)
+        val radioButtonGender: RadioGroup = findViewById(R.id.radioGroupGender)
         val checkBoxSmoker: CheckBox = findViewById(R.id.checkBoxSmoker)
 
-        // set event listener
+        textViewPremium = findViewById(R.id.textViewPremium)
+        textViewMalePremium = findViewById(R.id.textViewMalePremium)
+        textViewSmokerPremium = findViewById(R.id.textViewSmokerPremium)
+        textViewTotal = findViewById(R.id.textViewTotal)
+
+                // set event listener
         spinnerAge.onItemSelectedListener = this
-        radioButtonMale.setOnClickListener {
-            val male = it.isSelected // it refers to the current view
-            if(male) {
-                    //TODO: calculate male premium
+        radioButtonGender.setOnCheckedChangeListener() { it, _ ->
+            val male = it.checkedRadioButtonId // it refers to the current view
+            malePremium = if(male == R.id.radioButtonMale) {
+                val age = spinnerAge.selectedItemPosition
+                when (age) {
+                    1 -> 50
+                    2 -> 100
+                    3 -> 150
+                    4, 5 -> 200
+                    else -> 0
                 }
+            } else
+                0
+            textViewMalePremium.text = malePremium.toString()
         }
 
         checkBoxSmoker.setOnCheckedChangeListener{ compoundButton, b ->
             if(compoundButton.isChecked) {
-                // TODO: calculate smoker premium
+                val age = spinnerAge.selectedItemPosition
+                smokerPremium = when (age) {
+                    1 -> 100
+                    2 -> 150
+                    3 -> 200
+                    4 -> 250
+                    5 -> 300
+                    else -> 0
+                }
+            } else {
+                smokerPremium = 0
             }
-
+            textViewSmokerPremium.text = smokerPremium.toString()
         }
 
+        val buttonCalculate: Button = findViewById(R.id.buttonCalculate)
+            buttonCalculate.setOnClickListener {
+                total = premium + malePremium + smokerPremium
+                textViewPremium.text = premium.toString()
+                textViewMalePremium.text = malePremium.toString()
+                textViewSmokerPremium.text = smokerPremium.toString()
+                textViewTotal.text = total.toString()
     }
-    //TODO: calculate total
 
-    //fun calculatePremium(view: View) {
-        //val radioGroupGender: RadioGroup = findViewById(R.id.radioGroupGender)
-        //val gender = radioGroupGender.checkedRadioButtonId
-        //if (gender == R.id.radioButtonMale) {
-            //TODO: calculate male premium
-        //}
-    //}
+        val buttonReset: Button = findViewById(R.id.buttonReset)
+        buttonReset.setOnClickListener {
+            spinnerAge.setSelection(0)
+            radioButtonGender.clearCheck()
+            textViewPremium.text = null
+            textViewMalePremium.text = null
+            textViewSmokerPremium.text = null
+            textViewTotal.text = null
+        }
+}
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, index: Int, id: Long) {
         //Toast.makeText(this, "Item selected = ${index}", Toast.LENGTH_SHORT).show()
@@ -54,11 +93,12 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             0 -> premium = 60
             1 -> premium = 70
             2 -> premium = 80
-            //TODO: complete the assignment of basic premium
+            3 -> premium = 120
+            4,5 -> premium = 150
         }
+        textViewPremium.text = premium.toString()
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
-        TODO("Not yet implemented")
     }
 }
